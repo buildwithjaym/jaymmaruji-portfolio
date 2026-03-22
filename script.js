dcea01b86845aa8fe0b931f10c0d7b8358bb88d7
Jaymar Maruji <jaymmaruji@gmail.com>
2026-03-22 13:56:11 +0800

    Added the Typing effect and code it

---
+++ script.js
@@ -0,0 +1,11 @@
+AOS.init({duration:1000, once:true})
+
+// The typing effect
+new Typed("#typing", {
+    strings: ['Ui/UX Designer.', 'Web Developer.', 'Creative Thinker.'],
+    typeSpeed: 60,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop: true
+
+});
