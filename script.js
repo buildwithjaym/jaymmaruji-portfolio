4672480e83a36bcf8a34cfce3df3182cce1ef867
Jaymar Maruji <jaymmaruji@gmail.com>
2026-03-30 09:41:17 +0800

    added my name on the typing

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ AOS.init({duration:1000, once:true})
 
 // The typing effect
 new Typed("#typing", {
-    strings: ['UI/UX Designer.', 'Web Developer.', 'Creative Thinker.', 'Building Solution.'],
+    strings: ['Jaymar H. Maruji','UI/UX Designer.', 'Web Developer.', 'Creative Thinker.', 'Building Solution.'],
     typeSpeed: 60,
     backSpeed: 30,
     backDelay: 1000,
