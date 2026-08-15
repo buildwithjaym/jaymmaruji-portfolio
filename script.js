75bd6c1d42c8fbe8d8d26f52c8578b1ab3047745
buildwithjaym <jaymmaruji@gmail.com>
2026-08-15 21:55:50 +0800

    we added a other and it will opena new text message

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-AOS.init({duration:1000, once:true})
+AOS.init({ duration: 1000, once: true })
 
 // The typing effect
 new Typed("#typing", {
-    strings: ['Jaymar H. Maruji','UI/UX Designer.', 'Web Developer.', 'Creative Thinker.', 'Building Solution.'],
+    strings: ['Jaymar H. Maruji', 'UI/UX Designer.', 'Web Developer.', 'Creative Thinker.', 'Building Solution.'],
     typeSpeed: 60,
     backSpeed: 30,
     backDelay: 1000,
     loop: true
+
+});
+
+const serviceSelect = document.getElementById("service_type");
+const otherService = document.getElementById("other_service");
+
+serviceSelect.addEventListener("change", function () {
+
+    if (this.value === "Other") {
+
+        otherService.style.display = "block";
+        otherService.required = true;
+
+    } else {
+
+        otherService.style.display = "none";
+        otherService.required = false;
+        otherService.value = "";
+
+    }
 
 });
 
